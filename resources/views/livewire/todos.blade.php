37aea924c4db21827ddac73ddd66a2a594ec1ebb
luhw-dev <luhw.dev@gmail.com>
2024-05-22 18:48:23 -0300

    fixed but not pretty.

--- resources/views/livewire/todos.blade.php
+++ resources/views/livewire/todos.blade.php
@@ -2,7 +2,7 @@
 
 use function Livewire\Volt\{state, with};
 
-state(['task','edit']);
+state(['task', 'todo', 'editTask']);
 
 with([
     'todos' => fn() => auth()->user()->todos,
@@ -17,12 +17,15 @@ $add = function () {
 
 $delete = fn(\App\Models\Todo $todo) => $todo->delete();
 
+$edit = function (\App\Models\Todo $todo) {
+    $this->editTask = $todo->task;
+    $this->todo = $todo;
+};
 
-$edit = function ($todoId) {
-    $todo = \App\Models\Todo::findOrFail($todoId);
-    $todo->update([
-        'task' => $this->edit
-    ]);
+$update = function () {
+    $this->todo->task = $this->editTask;
+    $this->todo->save();
+    $this->dispatch('close-modal', 'edit-task-modal');
 };
 ?>
 
@@ -41,8 +44,7 @@ $edit = function ($todoId) {
                 <span>{{ $todo->task }}</span>
                 <div class="flex">
                     <button class="text-gray-400 hover:text-gray-500 focus:outline-none focus:text-gray-500"
-                        wire:click="edit({{ $todo->id }})"
-                        x-on:click.prevent="$dispatch('open-modal', 'edit-task-modal')">
+                        wire:click="edit({{ $todo->id }})" x-on:click.prevent="$dispatch('open-modal', 'edit-task-modal')">
                         <x-heroicon-o-pencil-square class="h-5 w-5" />
                     </button>
                     <button class="text-gray-400 hover:text-gray-500 focus:outline-none focus:text-gray-500 ml-2"
@@ -53,11 +55,16 @@ $edit = function ($todoId) {
             </div>
         @endforeach
     </div>
-    <x-modal maxWidth="sm" class="bg-slate-800" name="edit-task-modal" :show="$errors->isNotEmpty()" focusable>
-        <form wire:submit.prevent="edit($editId)">
-            <x-input-label for="title_edit" :value="__('Title')" />
-            <x-text-input type="text" id="title_edit" wire:model="edit"/>
-            <x-secondary-button type="submit">Save</x-secondary-button>
-        </form>
+    <x-modal maxWidth="sm" class=" bg-slate-800" name="edit-task-modal" :show="$errors->isNotEmpty()" focusable>
+        <section class="p-10">
+            <form wire:submit.prevent="update()">
+                <x-input-label for="title_edit" :value="__('Title')" />
+                <x-text-input type="text" id="title_edit" wire:model="editTask" />
+                <!-- // sua missao eh descobrir como matar esse editTask e usar o todo.task pra ficar mais bonito
+            //  esse jeito resolve mas por algum motivo que nao sei o qual agora, nao ta funcionando o todo.task
+            // eh como se nao tivesse entrando nas propriedades do model definido no state -->
+                <x-secondary-button type="submit">Save</x-secondary-button>
+            </form>
+        </section>
     </x-modal>
 </div>
